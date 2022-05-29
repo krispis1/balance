d400329e519974f56dd0121634735bc965c59c91
krispis1 <belickaskristupas@outlook.com>
2022-05-29 09:50:30 +0300

    Project init

---
+++ src/test/java/lt/inventi/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.inventi.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
