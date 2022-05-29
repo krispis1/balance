56b688b3020439056b2f1bd4018049d34778a1a3
krispis1 <belickaskristupas@outlook.com>
2022-05-29 10:12:48 +0300

    Test controller for functionality testing

---
+++ src/main/java/lt/inventi/balance/controller/PingController.java
@@ -0,0 +1,13 @@
+package lt.inventi.balance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+}
