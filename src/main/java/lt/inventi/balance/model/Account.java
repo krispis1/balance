a13c3f7cbbe6ae45fba2ae1f486eef9d1adc4ee9
krispis1 <belickaskristupas@outlook.com>
2022-05-30 19:09:25 +0300

    Cleanup

--- src/main/java/lt/inventi/balance/model/Account.java
+++ src/main/java/lt/inventi/balance/model/Account.java
@@ -1,5 +1,6 @@
 package lt.inventi.balance.model;
 
+import lombok.Data;
 import lt.inventi.balance.util.CurrencyUtil.Currency;
 
 import javax.persistence.*;
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "accounts")
 public class Account {
@@ -24,35 +26,6 @@ public class Account {
     @Enumerated(EnumType.STRING)
     @NotNull
     private Currency currency;
-
     @OneToMany(mappedBy="account")
     private Set<Statement> statements;
-
-    public Integer getAccountId() {
-        return accountId;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
 }
--- src/main/java/lt/inventi/balance/model/Statement.java
+++ src/main/java/lt/inventi/balance/model/Statement.java
@@ -1,5 +1,6 @@
 package lt.inventi.balance.model;
 
+import lombok.Data;
 import lt.inventi.balance.util.CurrencyUtil.Currency;
 
 import javax.persistence.*;
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Table(name = "statements")
 public class Statement {
@@ -32,68 +34,7 @@ public class Statement {
     @Enumerated(EnumType.STRING)
     @NotNull
     private Currency currency;
-
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name="account_id", nullable=false)
     private Account account;
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public Integer getStatementId() {
-        return statementId;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public Timestamp getOperationTime() {
-        return operationTime;
-    }
-
-    public void setOperationTime(Timestamp operationTime) {
-        this.operationTime = operationTime;
-    }
-
-    public String getBeneficiary() {
-        return beneficiary;
-    }
-
-    public void setBeneficiary(String beneficiary) {
-        this.beneficiary = beneficiary;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
 }
